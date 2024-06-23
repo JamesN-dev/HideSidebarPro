21e0d890bf5784b687a78a3254349e081de9cd83
JamesN-dev <james@jamesn.dev>
2024-06-23 18:10:01 -0500

    • Changed Show Servers button to an icon in toolbar • Hover now working smoothly • Small server now working as intended • Settings updated, still need to fix

--- HideSidebarPro.plugin.js
+++ HideSidebarPro.plugin.js
@@ -1,10 +1,10 @@
 /**
  * @name HideSidebarPro
- * @version 1.0.0
- * @description A plugin to hide the sidebar in Discord.
+ * @version 0.0.2
+ * @description Hides the sidebar and reveals on hover. Toggle server list with icon in top right.
  * @author atetrax
  * @authorLink https://github.com/JamesN-dev
- * @invite YourInviteCode
+ * @authorid 204011638695657472
  * @donate https://www.buymeacoffee.com/s5mwQYovSz
  * @website https://github.com/JamesN-dev/HideSidebarPro
  * @source https://github.com/JamesN-dev/HideSidebarPro/blob/main/HideSidebarPro.plugin.js
@@ -14,13 +14,10 @@
 module.exports = class HideSidebarPro {
     constructor() {
         this.defaultSettings = {
-            forceProvider: false,
-            enableHoverArea: true,
-            hoverDelay: 300,
-            sidebarWidth: 240,
             buttonText: 'Hide Servers',
-            buttonPosition: 'top-right',
-            smallSidebar: false
+            smallServerList: false,
+            removeButton: false,
+            hideServers: true
         };
         this.settings = this.loadSettings();
     }
@@ -28,23 +25,20 @@ module.exports = class HideSidebarPro {
     start() {
         console.log('HideSidebarPro initializing...');
 
-        if (this.settings.enableHoverArea) {
-            this.addHoverArea();
+        if (!this.settings.removeButton) {
+            this.addHideServersButton();
         }
-        this.addHideServersButton();
         this.applyInitialStyles();
-        this.hideSidebar(); // Hide sidebar on startup
+        if (this.settings.hideServers) {
+            this.hideSidebar(); // Hide sidebar on startup
+        }
 
         console.log('HideSidebarPro initialized.');
     }
 
     stop() {
         BdApi.clearCSS('HideSidebarStyles');
-        BdApi.clearCSS('SmallSidebarStyles');
-        const hoverArea = document.getElementById('hover-area');
-        if (hoverArea) {
-            hoverArea.remove();
-        }
+        BdApi.clearCSS('SmallServerListStyles');
         const hideButton = document.getElementById('hds-btn');
         if (hideButton) {
             hideButton.remove();
@@ -52,69 +46,46 @@ module.exports = class HideSidebarPro {
         document.body.classList.remove('channel-hide'); // Ensure the sidebar is shown when the plugin stops
     }
 
-    addHoverArea() {
-        const hoverArea = document.createElement('div');
-        hoverArea.id = 'hover-area';
-        document.body.appendChild(hoverArea);
-
-        let hoverTimer;
-
-        hoverArea.addEventListener('mouseenter', () => {
-            hoverTimer = setTimeout(() => {
-                this.showSidebar();
-            }, this.settings.hoverDelay); // Use hover delay from settings
-        });
-
-        hoverArea.addEventListener('mouseleave', () => {
-            clearTimeout(hoverTimer);
-            this.hideSidebar();
-        });
-
-        const sidebar = document.querySelector('[class*="sidebar"]');
-        sidebar.addEventListener('mouseleave', (event) => {
-            if (!hoverArea.contains(event.relatedTarget)) {
-                this.hideSidebar();
-            }
-        });
-    }
-
     addHideServersButton() {
-        const button = document.createElement('button');
-        button.id = 'hds-btn';
-        button.textContent = this.settings.buttonText; // Use button text from settings
-        button.style.position = 'absolute';
-        switch (this.settings.buttonPosition) {
-            case 'top-left':
-                button.style.top = '8px';
-                button.style.left = '7px';
-                break;
-            case 'top-right':
-                button.style.top = '8px';
-                button.style.right = '7px';
-                break;
-            case 'bottom-left':
-                button.style.bottom = '8px';
-                button.style.left = '7px';
-                break;
-            case 'bottom-right':
-                button.style.bottom = '8px';
-                button.style.right = '7px';
-                break;
-        }
-
-        button.onclick = () => {
-            const guildsWrapper = document.querySelector('[class*="guilds"]');
-            if (guildsWrapper) {
-                if (guildsWrapper.style.display === 'none') {
-                    guildsWrapper.style.display = '';
-                    button.textContent = this.settings.buttonText;
-                } else {
-                    guildsWrapper.style.display = 'none';
-                    button.textContent = 'Show Servers';
+        const toolbar = document.querySelector('.toolbar_e44302');
+        if (toolbar) {
+            const buttonWrapper = document.createElement('div');
+            buttonWrapper.className = 'iconWrapper_e44302 clickable_e44302';
+            buttonWrapper.setAttribute('role', 'button');
+            buttonWrapper.id = 'toolbar-hds-btn';            
+            buttonWrapper.setAttribute('aria-label', 'Toggle Servers');
+            buttonWrapper.setAttribute('aria-expanded', 'false');
+            buttonWrapper.setAttribute('tabindex', '0');
+    
+            const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            svg.setAttribute('x', '0');
+            svg.setAttribute('y', '0');
+            svg.setAttribute('aria-hidden', 'true');
+            svg.setAttribute('role', 'img');
+            svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+            svg.setAttribute('width', '24');
+            svg.setAttribute('height', '24');
+            svg.setAttribute('fill', 'none');
+            svg.setAttribute('viewBox', '0 0 24 24');
+    
+            const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            path.setAttribute('fill', 'currentColor');
+            path.setAttribute('d', 'M12 2.81a1 1 0 0 1 0-1.41l.36-.36a1 1 0 0 1 1.41 0l9.2 9.2a1 1 0 0 1 0 1.4l-.7.7a1 1 0 0 1-1.3.13l-9.54-6.72a1 1 0 0 1-.08-1.58l1-1L12 2.8ZM12 21.2a1 1 0 0 1 0 1.41l-.35.35a1 1 0 0 1-1.41 0l-9.2-9.19a1 1 0 0 1 0-1.41l.7-.7a1 1 0 0 1 1.3-.12l9.54 6.72a1 1 0 0 1 .07 1.58l-1 1 .35.36ZM15.66 16.8a1 1 0 0 1-1.38.28l-8.49-5.66A1 1 0 1 1 6.9 9.76l8.49 5.65a1 1 0 0 1 .27 1.39ZM17.1 14.25a1 1 0 1 0 1.11-1.66L9.73 6.93a1 1 0 0 0-1.11 1.66l8.49 5.66Z');
+            path.setAttribute('clip-rule', 'evenodd');
+            svg.appendChild(path);
+    
+            buttonWrapper.appendChild(svg);
+            toolbar.appendChild(buttonWrapper);
+    
+            buttonWrapper.onclick = () => {
+                const guildsWrapper = document.querySelector('[class*="guilds"]');
+                if (guildsWrapper) {
+                    const isExpanded = buttonWrapper.getAttribute('aria-expanded') === 'true';
+                    buttonWrapper.setAttribute('aria-expanded', String(!isExpanded));
+                    guildsWrapper.style.display = isExpanded ? '' : 'none';
                 }
-            }
-        };
-        document.body.appendChild(button);
+            };
+        }
     }
 
     applyInitialStyles() {
@@ -125,7 +96,7 @@ module.exports = class HideSidebarPro {
                 padding-left: 3.5vmin !important;
             }
             body.channel-hide div[class*="sidebar"]:hover {
-                width: ${this.settings.sidebarWidth}px !important; // Use sidebar width from settings
+                width: 240px !important;
                 padding-left: 0 !important;
             }
 
@@ -141,35 +112,70 @@ module.exports = class HideSidebarPro {
                 transition: opacity 0.25s linear;
             }
 
-            #hover-area {
-                position: absolute;
-                left: 72px; /* Adjust width of the guilds bar */
-                top: 0;
-                bottom: 0;
-                width: 10px; /* Hover area width */
-                z-index: 1000;
-                background: transparent; /* Ensure the hover area is invisible */
-            }
-
             #hds-btn {
                 display: block;
                 background: #4f5660;
                 color: white;
-                padding-top: 4px;
-                padding-bottom: 4px;
+                padding-top: 5px;
+                padding-bottom: 5px;
                 z-index: 100;
-                border-radius: 3px;
-                width: 80px;
+                border-radius: 4px;
+                width: 100px; // Set button width
+            }
+
+            body.channel-hide .noticeInfo-3_iTE1,
+            .notice-2FJMB4 {
+                z-index: 1000;
+            }
+
+            body.channel-hide div[class*="toolbar-"] {
+                padding-right: 100px;
+            }
+
+            body.channel-hide .container-3gCOGc {
+                z-index: 0;
+            }
+
+            body.channel-hide #hds-btn {
+                display: block;
+                background: #4f5660;
+                color: white;
+                padding-top: 5px;
+                padding-bottom: 5px;
+                position: absolute;
+                top: 10px;
+                right: 9px;
+                z-index: 100;
+                border-radius: 4px;
+                width: 90px;
+            }
+
+            body.channel-hide.channel-hide div[class*="sidebar"] {
+                transition: all 0.1s ease 0.1s;
+                width: 0 !important;
+                padding-left: 3.5vmin !important;
+            }
+            body.channel-hide.channel-hide div[class*="sidebar"]:hover {
+                width: 240px !important;
+                padding-left: 0 !important;
+            }
+
+            body.channel-hide.channel-hide div[class*="sidebar"] > * {
+                visibility: hidden;
+                opacity: 0;
+                transition: visibility 0s 0.2s, opacity 0.2s linear;
+            }
+
+            body.channel-hide.channel-hide div[class*="sidebar"]:hover > * {
+                visibility: visible;
+                opacity: 1;
+                transition: opacity 0.4s linear;
             }
         `;
         BdApi.injectCSS('HideSidebarStyles', style);
 
-        if (this.settings.smallSidebar) {
-            const smallSidebarStyle = `
-                div[class^="sidebar"] {
-                    width: 160px !important;
-                }
-
+        if (this.settings.smallServerList) {
+            const smallServerListStyle = `
                 nav[aria-label="Servers sidebar"] {
                     display: flex;
                     width: 55px;
@@ -189,24 +195,9 @@ module.exports = class HideSidebarPro {
                 .wrapper_c5f96a {
                     height: 35px !important;
                     width: 35px !important;
-                }  
-
-                section[aria-label="User area"] div[class^="container"] {
-                    height: 76px;
-                    font-size: 14px;
-                    font-weight: 500;
-                    display: flex;
-                    padding: 0 8px 1px;
-                    flex-shrink: 0;
-                    position: relative;
-                    flex-direction: column-reverse;
-                    flex-wrap: wrap;
-                    align-content: flex-start;
-                    align-items: flex-start;
-                    justify-content: center;
                 }
             `;
-            BdApi.injectCSS('SmallSidebarStyles', smallSidebarStyle);
+            BdApi.injectCSS('SmallServerListStyles', smallServerListStyle);
         }
     }
 
@@ -226,7 +217,7 @@ module.exports = class HideSidebarPro {
             sidebar.classList.add('visible');
             document.body.classList.remove('channel-hide');
         } else {
-            console.error('Sidebar element not found.');
+            console.error('        sidebar element not found.');
         }
     }
 
@@ -238,10 +229,15 @@ module.exports = class HideSidebarPro {
         BdApi.Data.save("HideSidebarPro", "settings", this.settings);
     }
 
+    applySettings() {
+        this.stop();
+        this.start();
+    }
+
     getSettingsPanel() {
         const panel = document.createElement("div");
 
-        const createSetting = (name, key, type, callback) => {
+        const createSetting = (name, key, type) => {
             const settingDiv = document.createElement("div");
             settingDiv.className = "setting";
 
@@ -255,8 +251,6 @@ module.exports = class HideSidebarPro {
                 input.checked = this.settings[key];
                 input.onchange = (e) => {
                     this.settings[key] = e.target.checked;
-                    this.saveSettings();
-                    if (callback) callback();
                 };
             } else {
                 input = document.createElement("input");
@@ -264,8 +258,6 @@ module.exports = class HideSidebarPro {
                 input.value = this.settings[key];
                 input.onchange = (e) => {
                     this.settings[key] = type === "number" ? parseInt(e.target.value) : e.target.value;
-                    this.saveSettings();
-                    if (callback) callback();
                 };
             }
 
@@ -274,13 +266,19 @@ module.exports = class HideSidebarPro {
             return settingDiv;
         };
 
-//        panel.appendChild(createSetting("Force Provider", "forceProvider", "checkbox")); // Not fully implemented yet
-        panel.appendChild(createSetting("Enable Hover Area", "enableHoverArea", "checkbox", () => location.reload()));
-        panel.appendChild(createSetting("Hover Delay (ms)", "hoverDelay", "number"));
-//        panel.appendChild(createSetting("Sidebar Width (px)", "sidebarWidth", "number")); // Not fully implemented yet
         panel.appendChild(createSetting("Button Text", "buttonText", "text"));
-//        panel.appendChild(createSetting("Button Position", "buttonPosition", "text")); // Not fully implemented yet
-//        panel.appendChild(createSetting("Small Sidebar", "smallSidebar", "checkbox", () => location.reload())); // Not fully implemented yet
+        panel.appendChild(createSetting("Small Server List", "smallServerList", "checkbox"));
+        panel.appendChild(createSetting("Hide Server Button", "removeButton", "checkbox"));
+        panel.appendChild(createSetting("Hide Servers", "hideServers", "checkbox"));
+
+        const reloadButton = document.createElement("button");
+        reloadButton.textContent = "Reload Discord";
+        reloadButton.onclick = () => {
+            this.saveSettings();
+            location.reload();
+        };
+
+        panel.appendChild(reloadButton);
 
         return panel;
     }
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # HideSidebarPro
+Description
+
+HideSidebarPro is a alpha BetterDiscord plugin that enhances your Discord experience by allowing you to hide the channel sidebar and/or server list.
+
+Still in alpha stages. Please report any bugs to james@jamesn.design.
+
+Features    
+    •	Hide Sidebar with hover: Quickly hide and show the sidebar by hovering over the channel sidebar area.
+    •   Turn server list on or off with an icon located next to the inbox and help icons in upper right.    (still in development - Disappears when you click a channel or server.) 
+    •	Small Sidebar Option: Toggle a smaller sidebar in the settings for a more compact view.
+
+Installation
+
+1.	Download the Plugin:
+    •	Download the latest version of HideSidebarPro.plugin.js from the releases page.
+2.	Place in Plugin Folder:
+    •	Move the downloaded HideSidebarPro.plugin.js file to your BetterDiscord plugins folder:
+
+Windows: %AppData%/BetterDiscord/plugins
+macOS: ~/Library/Application Support/BetterDiscord/plugins
+Linux: ~/.config/BetterDiscord/plugins
+
+
+Acknowledgments
+
+Special thanks to patrickxchong for their work.
+This plugin was inspired by and based on the hide-discord-sidebar Chrome and Firefox extension available on GitHub. 
+https://github.com/patrickxchong/hide-discord-sidebar/tree/master
+
+
+Contributing
+
+Contributions are welcome! Feel free to open an issue or submit a pull request on GitHub.
+
+License
+
+This project is licensed under the GNU General Public License v3.0 - see the LICENSE file for details.
